c66d549fb412a0fb6aa9c667667cf187ed27b14d
liuziyuan <liuziyuan.room@gmail.com>
2024-08-16 15:22:25 +0800

    add comments

--- src/main/java/io/github/easyretrofit/core/RetrofitResourceContextBuilder.java
+++ src/main/java/io/github/easyretrofit/core/RetrofitResourceContextBuilder.java
@@ -65,7 +65,6 @@ public class RetrofitResourceContextBuilder {
         return new RetrofitResourceClassBean(retrofitBuilderClassSet);
     }
 
-
     public List<RetrofitClientBean> getRetrofitClientBeanList() {
         return retrofitClientBeanList;
     }
