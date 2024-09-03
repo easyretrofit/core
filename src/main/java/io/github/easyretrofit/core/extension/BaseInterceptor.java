a8f5008074aa72fce9c97fd3b5d6a6ca978d0ca1
liuziyuan <liuziyuan.room@gmail.com>
2024-09-03 17:25:10 +0800

    add getExtensionAnnotation function

--- src/main/java/io/github/easyretrofit/core/extension/BaseInterceptor.java
+++ src/main/java/io/github/easyretrofit/core/extension/BaseInterceptor.java
@@ -1,6 +1,7 @@
 package io.github.easyretrofit.core.extension;
 
 import io.github.easyretrofit.core.RetrofitResourceContext;
+import io.github.easyretrofit.core.resource.RetrofitApiInterfaceBean;
 import io.github.easyretrofit.core.util.AntPathMatcher;
 import io.github.easyretrofit.core.util.PathMatcher;
 import okhttp3.Interceptor;
@@ -9,7 +10,9 @@ import okhttp3.Response;
 import retrofit2.Invocation;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.LinkedHashSet;
 import java.util.Objects;
 
 /**
@@ -24,6 +27,7 @@ public abstract class BaseInterceptor implements Interceptor {
     private String[] include;
     private String[] exclude;
     private final PathMatcher pathMatcher = new AntPathMatcher();
+    private Chain chain;
 
 
     protected BaseInterceptor() {
@@ -35,6 +39,7 @@ public abstract class BaseInterceptor implements Interceptor {
 
     @Override
     public final Response intercept(Chain chain) throws IOException {
+        this.chain = chain;
         Request request = chain.request();
         String path = request.url().encodedPath();
         Method method = this.getRequestMethod(request);
@@ -81,7 +86,6 @@ public abstract class BaseInterceptor implements Interceptor {
         return false;
     }
 
-
     public void setInclude(String[] include) {
         this.include = include;
     }
@@ -100,6 +104,33 @@ public abstract class BaseInterceptor implements Interceptor {
 
     protected Method getRequestMethod(Request request) {
         return Objects.requireNonNull(request.tag(Invocation.class)).method();
+    }
+
+    protected Annotation getExtensionAnnotation(Class<? extends Annotation> annotationClazz) {
+        Annotation annotation = null;
+        if (chain != null) {
+            Request request = chain.request();
+            final Method method = this.getRequestMethod(request);
+            String clazzName = this.getClazzNameByMethod(method);
+            final RetrofitApiInterfaceBean currentServiceBean = context.getRetrofitApiInterfaceBean(clazzName);
+            for (Class<?> parentClass : currentServiceBean.getSelf2ParentClasses()) {
+                annotation = parentClass.getAnnotation(annotationClazz);
+                if (annotation != null && self2ParentHasAnnotation(currentServiceBean.getSelf2ParentClasses(), annotationClazz))
+                    return annotation;
+            }
+        }
+        return annotation;
+    }
+
+    private boolean self2ParentHasAnnotation(LinkedHashSet<Class<?>> self2ParentClasses, Class<? extends Annotation> annotationClazz) {
+        for (Class<?> clazz : self2ParentClasses) {
+            RetrofitApiInterfaceBean retrofitApiInterfaceBean = context.getRetrofitApiInterfaceBean(clazz);
+            Annotation annotationResource = retrofitApiInterfaceBean.getAnnotationResource(annotationClazz);
+            if (annotationResource != null && annotationResource.annotationType().equals(annotationClazz)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
