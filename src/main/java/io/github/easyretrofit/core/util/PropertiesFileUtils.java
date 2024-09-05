6080652a1e6199347dd3921a14694a07227c89f3
liuziyuan <liuziyuan.room@gmail.com>
2024-09-05 10:36:42 +0800

    modify PropertiesFileUtils

--- src/main/java/io/github/easyretrofit/core/util/PropertiesFileUtils.java
+++ src/main/java/io/github/easyretrofit/core/util/PropertiesFileUtils.java
@@ -1,38 +1,33 @@
 package io.github.easyretrofit.core.util;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.util.HashSet;
+import java.util.Properties;
 import java.util.Set;
 
 public class PropertiesFileUtils {
-
     public static Set<String> getPropertiesKeys(Reader reader, String extensionClazzName) {
         Set<String> extensionNames = new HashSet<>();
-        try (BufferedReader bufferedReader = new BufferedReader(reader)) {
-            String line;
-            StringBuilder sb = new StringBuilder();
-            while ((line = bufferedReader.readLine()) != null) {
-                sb.append(line);
-            }
-            String finalStr = sb.toString().replaceAll("\\\\", "").trim();
-            String[] split = finalStr.split("=");
-            if (extensionClazzName.equalsIgnoreCase(split[0].trim())) {
-                String className = split[1].trim();
-                if (className.contains(",")) {
-                    String[] classNames = className.split(",");
-                    for (String classname : classNames) {
-                        extensionNames.add(getExtensionPackageName(classname));
+        Properties properties = new Properties();
+        try {
+            properties.load(reader);
+            for (String key : properties.stringPropertyNames()) {
+                String className = properties.getProperty(key);
+                if (extensionClazzName.equalsIgnoreCase(key)) {
+                    if (className.contains(",")) {
+                        String[] classNames = className.split(",");
+                        for (String classname : classNames) {
+                            extensionNames.add(getExtensionPackageName(classname));
+                        }
+                    } else {
+                        extensionNames.add(getExtensionPackageName(className));
                     }
-                } else {
-                    extensionNames.add(getExtensionPackageName(className));
                 }
             }
-            return extensionNames;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return extensionNames;
     }
 
     private static String getExtensionPackageName(String classname) {
