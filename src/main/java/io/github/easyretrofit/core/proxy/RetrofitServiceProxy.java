60b3e663800fd1df8dbea8bbd30e3b1f14febfa9
liuziyuan <liuziyuan.room@gmail.com>
2024-09-24 13:34:14 +0800

    modify proxy extension exception

--- src/main/java/io/github/easyretrofit/core/proxy/RetrofitServiceProxy.java
+++ src/main/java/io/github/easyretrofit/core/proxy/RetrofitServiceProxy.java
@@ -3,6 +3,7 @@ package io.github.easyretrofit.core.proxy;
 import io.github.easyretrofit.core.exception.RetrofitExtensionException;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Set;
 
@@ -28,21 +29,31 @@ public class RetrofitServiceProxy<T> implements InvocationHandler {
             return method.invoke(t, args);
         } catch (Exception e) {
             Throwable cause = e.getCause();
-            Object exObj = null;
             if (cause instanceof RetrofitExtensionException) {
-                for (BaseExceptionDelegate<? extends RetrofitExtensionException> exceptionDelegate : exceptionDelegates) {
-                    if (exceptionDelegate.getExceptionClassName().equals(cause.getClass())) {
-                        ExceptionDelegator<? extends Throwable> delegator = new ExceptionDelegator<>(exceptionDelegate);
-                        Object invoke = delegator.invoke(proxy, method, args, (RetrofitExtensionException) cause);
-                        if (invoke != null) {
-                            exObj = invoke;
-                            break;
-                        }
-                    }
-                }
-                return exObj;
+                return getProxyExceptionObject(proxy, method, args, cause);
             }
+//            else {
+//                Throwable targetCause = ((InvocationTargetException) e).getTargetException();
+//                if (targetCause instanceof RetrofitExtensionException) {
+//                    return getProxyExceptionObject(proxy, method, args, targetCause);
+//                }
+//            }
             throw cause;
         }
+    }
+
+    private Object getProxyExceptionObject(Object proxy, Method method, Object[] args, Throwable cause) {
+        Object exObj = null;
+        for (BaseExceptionDelegate<? extends RetrofitExtensionException> exceptionDelegate : exceptionDelegates) {
+            if (exceptionDelegate.getExceptionClassName().isAssignableFrom(cause.getClass())) {
+                ExceptionDelegator<? extends Throwable> delegator = new ExceptionDelegator<>(exceptionDelegate);
+                Object invoke = delegator.invoke(proxy, method, args, (RetrofitExtensionException) cause);
+                if (invoke != null) {
+                    exObj = invoke;
+                    break;
+                }
+            }
+        }
+        return exObj;
     }
 }
