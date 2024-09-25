8970d0351ae062102085cb49b96ad7c842d520fa
liuziyuan <liuziyuan.room@gmail.com>
2024-09-25 14:36:25 +0800

    remove cglib

--- src/main/java/io/github/easyretrofit/core/proxy/CglibDynamicProxy.java
+++ src/main/java/io/github/easyretrofit/core/proxy/CglibDynamicProxy.java
@@ -1,15 +1,15 @@
-package io.github.easyretrofit.core.proxy;
-
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
-
-public class CglibDynamicProxy {
-
-    public static <T> T create(Class<?> clazz,
-                               MethodInterceptor invocationHandler) {
-        Enhancer enhancer = new Enhancer();
-        enhancer.setSuperclass(clazz);
-        enhancer.setCallback(invocationHandler);
-        return (T) enhancer.create();
-    }
-}
+//package io.github.easyretrofit.core.proxy;
+//
+//import net.sf.cglib.proxy.Enhancer;
+//import net.sf.cglib.proxy.MethodInterceptor;
+//
+//public class CglibDynamicProxy {
+//
+//    public static <T> T create(Class<?> clazz,
+//                               MethodInterceptor invocationHandler) {
+//        Enhancer enhancer = new Enhancer();
+//        enhancer.setSuperclass(clazz);
+//        enhancer.setCallback(invocationHandler);
+//        return (T) enhancer.create();
+//    }
+//}
